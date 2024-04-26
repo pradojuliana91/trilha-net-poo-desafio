2b7bfa652cd576bb762f1a5131085c9cb727597d
Juliana Prado <ju.pradoo@gmail.com>
2024-04-26 10:23:23 -0300

    Desafio projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iph = new Iphone("(16)98304503","Iphone 11","23443759603945",128);
+iph.Ligar();
+iph.InstalarAplicativo("Instagram");
+iph.ReceberLigacao();
+Console.WriteLine("");
+Nokia nk =new Nokia("(11)997650394","Nokia G60 5G","2334567830495823",256);
+nk.Ligar();
+nk.InstalarAplicativo("Shopee");
+nk.ReceberLigacao();
